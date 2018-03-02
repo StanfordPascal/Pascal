68603a2c7143e74418b70e3b42c188a765a5613c
Bernd Oppolzer <berndoppolzer@yahoo.com>
2018-03-02 21:37:22 +0100

    Modified MVS installation inspired by conversation with moshix (see YouTube)

--- mvsinst/submvs.cmd
+++ mvsinst/submvs.cmd
@@ -2,11 +2,13 @@
 if "%1" == "" goto :noparm
 if not exist %1 goto :fehler
 nc -w1 127.0.0.1 3505 <%1
+echo Job %1 submitted to TK4- successfully
 goto :fertig
 :fehler
-echo Datei %1 nicht vorhanden
+echo File %1 does not exist
 goto :fertig
 :noparm
-echo usage SUBMVS dateiname (fertiger Job fr TK4- MVS)
+echo Submit Job to TK4- MVS
+echo one parameter needed: name of file containing JCL
 goto :fertig
 :fertig
